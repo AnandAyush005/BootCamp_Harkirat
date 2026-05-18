240a09ea86c7a9f366a6907929e4a475256a1f3a
anandayush005 <anandayush233@gmail.com>
2026-05-18 13:11:56 +0530

    Solved question 6,7,8

--- web_dev_and_devops/week2/greet.js
+++ web_dev_and_devops/week2/greet.js
@@ -1,11 +1,14 @@
 function greet(user){
 
     console.log("Hi " + user.name + ", I think your age is : " + user.age);
+
+    if(user.age >= 18) console.log("You are elidigible to vote");
+    else console.log("You are not elidigible to vote");
 }
 
 let user1 = {
     name : "Ayush anand",
-    age : 19
+    age : 17
 }
 
 let user2 = {
